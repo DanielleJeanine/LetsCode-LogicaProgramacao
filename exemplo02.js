3af2e48ed2fabb5072bd10129e3ca00fb55387e2
DanielleJeanine <danielle.jeanine92@gmail.com>
2022-02-09 20:31:48 -0300

    Novos Exemplos

--- exemplo02.js
+++ exemplo02.js
@@ -1,6 +1,12 @@
 //Javascript -Linguagem de Programação
 
 //Armazenar Dados
-chave=valor
+// var chave=valor
+//let chave=valor;
+//const chave = valor;
+var escola = "Lets Code";
+let nome = "Danielle";
+const idade = 29;
+var alto = false;
 
 //Executar Tarefas
