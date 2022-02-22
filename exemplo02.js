a08557acf08866a1b019cd2c9dc36a2a2dfa6a21
DanielleJeanine <danielle.jeanine92@gmail.com>
2022-02-22 10:52:35 -0300

    Atualização

--- exemplo02.js
+++ exemplo02.js
@@ -1,6 +1,7 @@
 //Javascript -Linguagem de Programação
 
 //Armazenar Dados
+//Sintaxe
 // var chave=valor
 //let chave=valor;
 //const chave = valor;
@@ -10,3 +11,10 @@ const idade = 29;
 var alto = false;
 
 //Executar Tarefas
+//Sintaxe
+//comando();
+//comando(parametro1, parametro2);
+//alert("Boa noite")
+prompt();
+prompt("Digite seu nome");
+prompt("Digite sua idade", "Sua idade aqui");
---
+++ exemplo03.js
@@ -0,0 +1,14 @@
+//Armazanando dados
+var nome = "Luiz";
+var email = "luiz@gmail.com";
+var telefone = "99 1234-5678";
+
+//Executando intruções
+//contexto.comando(parametro1,parametro1,...);
+console.log("nome", nome);
+console.log("email", email);
+console.log("telefone", telefone);
+
+document.write("Nome: ", nome, "<br>");
+document.write("Email: ", email);
+document.write("<br>Telefone: ", telefone);
